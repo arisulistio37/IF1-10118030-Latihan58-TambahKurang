6b6d426bf57e4d83830a1be56b98d6be63c4934e
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:53:07 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan58/tambahkurang/IF110118030Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi program : program ini berisi tentang perhitungan tambah kurang
+ */
+public class IF110118030Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jBilangan = new JumlahBilangan();
+        jBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan sBilangan = new SelisihBilangan();
+        sBilangan.tampilSelisih();
+    }
+    
+}
